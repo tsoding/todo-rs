7ee0b367d61ca2b527bcb0989de2d31f15875422
rexim <reximkut@gmail.com>
2021-06-21 20:08:30 +0700

    More work

--- src/main.rs
+++ src/main.rs
@@ -91,6 +91,7 @@ fn list_transfer(list_dst: &mut Vec<String>, list_src: &mut Vec<String>, list_sr
 
 // TODO: persist the state of the application
 // TODO: add new items to TODO
+// TODO: delete items
 // TODO: edit the items
 // TODO: keep track of date when the item was DONE
 // TODO: undo system
