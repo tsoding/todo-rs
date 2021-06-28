61e3a32fea79ceb99847380139bbcfa214b9bd9e
rexim <reximkut@gmail.com>
2021-06-28 15:11:24 +0700

    Add TODO(#23)

--- src/ctrlc.rs
+++ src/ctrlc.rs
@@ -1,6 +1,6 @@
 use std::sync::atomic::{AtomicBool, Ordering};
 
-// TODO: ctrlc module is not implemented for windows
+// TODO(#23): ctrlc module is not implemented for windows
 // It's not that important right now, since ncurses crate already prevents it from working properly
 // on windows anyway.
 #[cfg(not(unix))]
